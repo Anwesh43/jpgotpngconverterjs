9c28103783e80535bdb82820fbf5f843536aad0d
Anwesh43 <anweshthecool0@gmail.com>
2017-05-10 14:39:43 +0530

    Adding JpegToPngConverter

---
+++ client/JpegToPngConverter.js
@@ -0,0 +1,32 @@
+class JpegToPngConverter {
+    constructor(src) {
+        this.src = src
+    }
+    createConvertedImage() {
+        const canvas = document.createElement('canvas')
+        const context = canvas.getContext('2d')
+        const image = new Image()
+        image.src = src
+        image.onload = () => {
+            canvas.width = image.width
+            canvas.height = image.height
+            context.drawImage(image,0,0,image.width,image.height)
+            let imgData = context.getImageData(0,0,canvas.width,canvas.height)
+            let data = imgData.data
+            if(data.length > 4 && data.length%4 == 0) {
+                const alpha = data[3],red = data[0],green = data[1],blue = data[2]
+                for(var i = 4;i<data.length;i+=4) {
+                    const currRed = data[i],currGreen = data[i+1],currBlue = data[i+2],currAlpha = data[i+3]
+                    if(red == currRed && green == currGreen && blue == currBlue && alpha == currAlpha) {
+                        data[i+3] = 0
+                    }
+                }
+                context.putImageData(imgData,0,0)
+                const imgDom = document.createElement('img')
+                imgDom.src = canvas.toDataURL()
+                document.body.appendChild(imgDom)
+            }
+        }
+        context.drawImage()
+    }
+}
